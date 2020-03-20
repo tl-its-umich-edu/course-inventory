16ed44874dadd55596fb7a622f24dcc3de708c30
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2020-03-20 14:00:34 -0400

    Fixed up formatting (flake8), moved settings, added vscode config

--- report.py
+++ report.py
@@ -1,20 +1,22 @@
 import requests
 # standard libraries
-import json, logging, os
+import json
+import logging
+import os
 from json.decoder import JSONDecodeError
-from typing import Dict, Sequence, Union
+from typing import Dict, Union
 
 # third-party libraries
 import pandas as pd
-from pandas.io.json import json_normalize
-import psycopg2
 import furl
 
 # Initialize settings and globals
 
 logger = logging.getLogger(__name__)
 
+
 # Functions
+<<<<<<< HEAD
 def get_total_page_count(url:str, headers: Dict[str, Union[str, int]] = {}):
   logger.info("get_total_page_count")
   # get the total page count
@@ -33,8 +35,24 @@ def get_total_page_count(url:str, headers: Dict[str, Union[str, int]] = {}):
         logger.info(f"url ={url} page count={total_page_count}")
       except JSONDecodeError:
         logger.warning('JSONDecodeError encountered')
+=======
+def get_total_page_count(url: str, headers: Dict[str, Union[str, int]] = {}):
+    # get the total page count
+    total_page_count = 1
+    response = requests.request("GET", f"{url}", headers=headers, data={})
+    status_code = response.status_code
+    if status_code != 200:
+        logger.warning(f'Received irregular status code: {status_code}')
+>>>>>>> Fixed up formatting (flake8), moved settings, added vscode config
         logger.info('No page at all!')
-  return total_page_count
+    else:
+        try:
+            results = json.loads(response.text.encode('utf8'))
+            total_page_count = results['page_count']
+        except JSONDecodeError:
+            logger.warning('JSONDecodeError encountered')
+            logger.info('No page at all!')
+    return total_page_count
 
 
 # read configurations
@@ -42,7 +60,8 @@ try:
     with open(os.path.join('config', 'env.json')) as env_file:
         ENV = json.loads(env_file.read())
 except FileNotFoundError:
-    logger.error('Configuration file could not be found; please add env.json to the config directory.')
+    logger.error(
+        'Configuration file could not be found; please add env.json to the config directory.')
 
 logging.basicConfig(level=ENV.get('LOG_LEVEL', 'DEBUG'))
 
@@ -57,48 +76,53 @@ headers = {
 
 
 def run_report(param_attribute, headers, json_attribute_name):
-  url = ENV[f'ZOOM_{param_attribute}_URL']
-  params = ENV[f'ZOOM_{param_attribute}_PARAMS']
 
-  # construct param string
-  f = furl.furl('')
-  f.args = params
+    url = ENV[f'ZOOM_{param_attribute}_URL']
+    params = ENV[f'ZOOM_{param_attribute}_PARAMS']
 
-  # get total page count
-  total_page_count = get_total_page_count(f"{url}{f.url}", headers)
+    # construct param string
+    f = furl.furl('')
+    f.args = params
+    print(f.url)
 
-  total_df = pd.DataFrame()
+    # get total page count
+    total_page_count = get_total_page_count(f"{url}{f.url}", headers)
 
-  while (params['page_number'] <= total_page_count):
-      logger.info(f"Page Number: {params['page_number']} out of total page number {total_page_count}")
+    total_df = pd.DataFrame()
 
-      # construct param string
-      f = furl.furl('')
-      f.args = params
-      
-      response = requests.request("GET", f"{url}{f.url}", headers=headers, data = payload)
-      status_code = response.status_code
-      if status_code != 200:
-        logger.warning(f'Received irregular status code: {status_code}')
-        logger.info('No more pages!')
-      else:
-          try:
-            results = json.loads(response.text.encode('utf8'))
+    while (params['page_number'] <= total_page_count):
+        logger.info(
+            f"Page Number: {params['page_number']} out of total page number {total_page_count}")
 
-            df = pd.DataFrame(results[json_attribute_name])
-            logger.info(df)
-            total_df = total_df.append(df)
-            logger.info(f'data frame size {len(total_df)}')
-            # go retrieve next page
-            params['page_number'] +=1
-            params['next_page_token'] = results['next_page_token']
-          except JSONDecodeError:
-              logger.warning('JSONDecodeError encountered')
-              logger.info('No more pages!')
+        # construct param string
+        f = furl.furl('')
+        f.args = params
+        print(f.url)
 
-  # output csv file
-  output_file_name = f"total_{json_attribute_name}.csv"
-  total_df.to_csv(output_file_name)
+        response = requests.request(
+            "GET", f"{url}{f.url}", headers=headers, data=payload)
+        status_code = response.status_code
+        if status_code != 200:
+            logger.warning(f'Received irregular status code: {status_code}')
+            logger.info('No more pages!')
+        else:
+            try:
+                results = json.loads(response.text.encode('utf8'))
+
+                df = pd.DataFrame(results[json_attribute_name])
+                logger.info(df)
+                total_df = total_df.append(df)
+                logger.info(f'data frame size {len(total_df)}')
+                # go retrieve next page
+                params['page_number'] += 1
+            except JSONDecodeError:
+                logger.warning('JSONDecodeError encountered')
+                logger.info('No more pages!')
+
+    # output csv file
+    output_file_name = f"total_{json_attribute_name}.csv"
+    total_df.to_csv(output_file_name)
+
 
 # run users report
 run_report('USERS', users_params, headers, 'users')
