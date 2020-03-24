db34c07c83e372b0e8fcd4c958c1e7e7da327a0d
pushyami <pushyami@umich.edu>
2020-03-24 12:30:10 -0400

    Refactoring the published date code to separate class

--- inventory.py
+++ inventory.py
@@ -9,8 +9,9 @@ import pandas as pd
 import psycopg2
 from requests import Response
 from umich_api.api_utils import ApiUtil
-from requests_futures.sessions import FuturesSession
-from concurrent.futures import as_completed
+from published_date.published_date import FetchPublishedDate
+
+
 
 # local libraries
 from db.db_creator import DBCreator
@@ -46,8 +47,6 @@ WAREHOUSE_INCREMENT = ENV['WAREHOUSE_INCREMENT']
 
 CREATE_CSVS = ENV.get('CREATE_CSVS', False)
 INVENTORY_DB = ENV['INVENTORY_DB']
-published_course_date = {}
-published_course_next_page_list = []
 
 # Function(s)
 
@@ -175,96 +174,6 @@ def check_if_valid_user_id(id: int, user_ids: Sequence[int]) -> bool:
         return False
 
 
-def get_next_page_url(response):
-    """
-    get the next page url from the Http response headers
-    :param response:
-    :type response: requests
-    :return: next_page_url
-    :rtype: str
-    """
-    logging.debug(get_next_page_url.__name__ + '() called')
-    results = response.result().links
-    if not results:
-        logging.debug('The api call do not have Link headers')
-        return None
-
-    if 'next' in results:
-        url_ = results['next']['url']
-        published_course_next_page_list.append(url_)
-        logger.info(f"Pagination size {len(published_course_next_page_list)} for published_at date")
-
-
-def published_date_resp_parsing(response):
-    logger.info("published_date_resp_parsing Call")
-
-    if response is None:
-        logger.info(f"Published course date response is None ")
-        return
-
-    start_time = time.time()
-
-    logger.info(f"published courses date collected so far : {len(published_course_date)}")
-    status = response.result().status_code
-    published_date_found = False
-    if status != 200:
-        logger.info(f"Response not successful with status code {status} due to {response.result().text}")
-        return
-
-    try:
-        audit_events = json.loads(response.result().text)
-    except JSONDecodeError as e:
-        logger.info(f"Error in parsing the response {e.message}")
-        return
-
-    if not audit_events:
-        logger.info(f"Response for fetching published date is empty {audit_events}")
-        return
-
-    events = audit_events['events']
-
-    for event in events:
-        if event['event_type'] == 'published':
-            course_id = event['links']['course']
-            published_date_found = True
-            published_course_date.update({course_id: event['created_at']})
-            logger.info(f"Published Date {event['created_at']} for course {course_id}")
-            break
-    if not published_date_found:
-        get_next_page_url(response)
-
-    seconds = time.time() - start_time
-    str_time = time.strftime("%H:%M:%S", time.gmtime(seconds))
-    logger.debug(f"Parsing the published date took {str_time} ")
-    return
-
-
-def get_published_course_date(course_ids, next_page_links=None):
-    logger.info("Starting of get_published_course_date call")
-    with FuturesSession(max_workers=NUM_ASYNC_WORKERS) as session:
-        headers = {'Content-type': 'application/json', 'Authorization': 'Bearer ' + CANVAS_TOKEN}
-        if next_page_links is not None:
-            logger.info("Going through Next page URL set")
-            responses = []
-            for next_page_link in next_page_links:
-                response = session.get(next_page_link, headers=headers)
-                published_course_next_page_list.remove(next_page_link)
-                responses.append(response)
-        else:
-            logger.info("Initial Round of Fetching course published date")
-            responses = [
-                session.get(f'{CANVAS_URL}/api/v1/audit/course/courses/{course_id}?per_page=100', headers=headers)
-                for course_id in course_ids]
-
-        for response in as_completed(responses):
-            published_date_resp_parsing(response)
-
-    if len(published_course_next_page_list) != 0:
-        logger.info(f"""Pagination size {len(published_course_next_page_list)} with published_at date items 
-                         {published_course_next_page_list}""")
-        get_published_course_date(course_ids, published_course_next_page_list)
-
-
 def run_course_inventory() -> None:
     start = time.time()
 
@@ -272,7 +181,9 @@ def run_course_inventory() -> None:
     course_df = gather_course_info_for_account(ACCOUNT_ID, TERM_ID)
     course_available_df = course_df.loc[course_df.workflow_state == 'available'].copy()
     course_available_ids = course_available_df['canvas_id'].to_list()
-    get_published_course_date(course_available_ids)
+    logger.info("**** Fetching the Published date ***")
+    published_dates = FetchPublishedDate(CANVAS_URL, CANVAS_TOKEN, NUM_ASYNC_WORKERS, course_available_ids)
+    published_course_date = published_dates.get_published_course_date(course_available_ids)
     course_published_date_df = pd.DataFrame(published_course_date.items(), columns=['canvas_id','published_at'])
     course_df = pd.merge(course_df, course_published_date_df, on='canvas_id', how='left')
     course_df['created_at'] = pd.to_datetime(course_df['created_at'],
