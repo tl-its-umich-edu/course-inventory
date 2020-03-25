94a2e06bfec86fabe2fd4f3a4dd9d70f2a2a16db
Sam Sciolla <ssciolla@umich.edu>
2020-03-25 15:49:00 -0400

    Change everything to sis_id

--- db/migrations/0004.add_um_id.py
+++
@@ -1,8 +0,0 @@
-#
-# file: migrations/0004.add_um_id.py
-#
-from yoyo import step
-
-__depends__ = {'0003.created_date_datatype_change'}
-
-step("ALTER TABLE user ADD column um_id INTEGER NULL AFTER name;")
