f6abfffe5bfc39130cdc2b128e3ebc82d3fe860c
Sam Sciolla <ssciolla@umich.edu>
2020-04-02 09:57:07 -0400

    Add manager WIP

--- course_inventory/inventory.py
+++ course_inventory/inventory.py
@@ -12,22 +12,13 @@ from umich_api.api_utils import ApiUtil
 
 # local libraries
 from db.db_creator import DBCreator
-from canvas.published_date import FetchPublishedDate
+from env import ENV
+from .published_date import FetchPublishedDate
 
 
 # Initialize settings and globals
 
 logger = logging.getLogger(__name__)
-
-try:
-    config_path = os.getenv("ENV_PATH", os.path.join('config', 'secrets', 'env.json'))
-    with open(config_path) as env_file:
-        ENV = json.loads(env_file.read())
-except FileNotFoundError:
-    logger.error('Configuration file could not be found; please add env.json to the config directory.')
-
-logging.basicConfig(level=ENV.get('LOG_LEVEL', 'DEBUG'),
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 ACCOUNT_ID = ENV.get('CANVAS_ACCOUNT_ID', 1)
 TERM_ID = ENV['CANVAS_TERM_ID']
@@ -184,8 +175,6 @@ def check_if_valid_user_id(id: int, user_ids: Sequence[int]) -> bool:
 
 
 def run_course_inventory() -> None:
-    start = time.time()
-
     # Gather course data
     course_df = gather_course_info_for_account(ACCOUNT_ID, TERM_ID)
     course_available_df = course_df.loc[course_df.workflow_state == 'available'].copy()
@@ -242,9 +231,7 @@ def run_course_inventory() -> None:
     logger.info('Emptying tables in DB')
     db_creator_obj = DBCreator(INVENTORY_DB)
     db_creator_obj.set_up()
-    if len(db_creator_obj.get_table_names()) > 0:
-        db_creator_obj.drop_records()
-    db_creator_obj.migrate()
+    db_creator_obj.drop_records()
     db_creator_obj.tear_down()
 
     # Insert gathered data
@@ -258,10 +245,7 @@ def run_course_inventory() -> None:
     enrollment_df.to_sql('enrollment', db_creator_obj.engine, if_exists='append', index=False)
     logger.info(f'Inserted data into enrollment table in {db_creator_obj.db_name}')
 
-    delta = time.time() - start
-    str_time = time.strftime("%H:%M:%S", time.gmtime(delta))
-    logger.info(f'Duration of run: {str_time}')
-
 
 if __name__ == "__main__":
+    logging.basicConfig(level=ENV.get('LOG_LEVEL', 'DEBUG'))
     run_course_inventory()
---
+++ run_jobs.py
@@ -0,0 +1,83 @@
+# standard libraries
+import json, logging, os, time
+from importlib import import_module
+from enum import Enum
+from typing import Dict, Sequence, Union
+
+# local libraries
+import course_inventory
+from db.db_creator import DBCreator
+from env import ENV
+import online_meetings
+
+
+# Initialize settings and global variables
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    level=ENV.get('LOG_LEVEL', 'DEBUG'),
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+
+# Enum(s)
+
+# Each job should be defined in AvailableJob.
+# NAME_OF_JOB = 'path.to.method'
+
+class AvailableJob(Enum):
+    COURSE_INVENTORY = 'course_inventory.inventory.run_course_inventory'
+    # ONLINE_MEETINGS = 'online_meetings.report...'
+    # ZOOM = 'online_meetings.canvas_zoom_meetings...'
+
+
+# Class(es)
+
+class Job:
+
+    def __init__(self, job: AvailableJob) -> None:
+        self.name = job.name
+        self.import_path = '.'.join(job.value.split('.')[:-1])
+        self.method_name = job.value.split('.')[-1]
+
+    def run(self):
+        leaf_module = import_module(self.import_path)
+        start_method = getattr(leaf_module, self.method_name)
+
+        # Until we have a decorator for this
+        start_time = time.time()
+        start_method()
+        delta = time.time() - start
+        str_time = time.strftime("%H:%M:%S", time.gmtime(delta))
+        logger.info(f'Duration of job run: {str_time}')
+
+
+class JobManager:
+
+    def __init__(self, job_names: Sequence[str]) -> None:
+        self.jobs = []
+        for job_name in job_names:
+            try:
+                avail_job = AvailableJob[job_name.upper()]
+                self.jobs.append(Job(avail_job))
+            except KeyError:
+                logger.error(f'{job_name} is not a valid job name')
+
+    def run_jobs(self):
+        for job in self.jobs:
+            logger.info(f'- - Running job {job.name} - -')
+
+            job.run()
+
+
+if __name__ == '__main__':
+    # Wait for MySQL to finish settting up
+    time.sleep(10.0)
+
+    # Apply any new migrations
+    db_creator_obj = DBCreator(ENV['INVENTORY_DB'])
+    db_creator_obj.migrate()
+
+    # Run those jobs
+    manager = JobManager(ENV['JOB_NAMES'])
+    manager.run_jobs()
