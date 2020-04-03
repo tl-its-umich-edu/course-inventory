b831ba20c77f9a4e024f049671daba736656890f
Zhen Qian <zqian@umich.edu>
2020-04-03 09:58:46 -0400

    issue_32 changes based on code reviews

--- online_meetings/bluejeans_report.py
+++ online_meetings/bluejeans_report.py
@@ -8,7 +8,6 @@ import yaml
 
 # third-party libraries
 import pandas as pd
-import furl
 import time
 import dateparser
 import pytz
@@ -22,12 +21,9 @@ from zipfile import ZipFile
 
 logger = logging.getLogger(__name__)
 
-# Default time to sleep when hitting a 429 limit
-DEFAULT_SLEEP_TIME = 10
-
 # read configurations
 try:
-    with open(os.path.join(os.path.dirname(__file__), '../config/env.json')) as env_file:
+    with open(os.path.join(os.path.dirname(__file__), '../config/secrets/env.json')) as env_file:
         ENV = yaml.safe_load(env_file.read())
 except FileNotFoundError:
     sys.exit(
@@ -56,17 +52,6 @@ expires_in_sec = expires_in_sec - 60
 token_expiration_time = datetime.now() + timedelta(seconds=expires_in_sec)
 logger.info(f"{token} expires on: {token_expiration_time}")
 
-# construct param string BLUEJEANS_MEETINGS_PARAMS
-
-params = ENV["BLUEJEANS_MEETINGS_PARAMS"]
-f = furl.furl('')
-f.args = params
-logger.info(f.url)
-
-bl_url = ENV["BLUEJEANS_MEETINGS_URL"]
-url = f'{bl_url}{f.url}'
-logger.info(url)
-
 payload = {}
 headers = {
   'Accept': 'application/json',
@@ -79,10 +64,8 @@ total_records = 0
 
 total_df = pd.DataFrame()
 
-params = ENV["BLUEJEANS_MEETINGS_PARAMS"]
-
 # TODO: the earliest time is hardcoded now
-early_date = dateparser.parse(ENV.get('ZOOM_EARLIEST_FROM', '2020-03-31')).date()
+early_date = dateparser.parse(ENV.get('ZOOM_EARLIEST_FROM', '2020-04-02')).date()
 
 # Only use the date as a paramter
 # Loop until yesterday (this will go until now() -1)
@@ -95,13 +78,13 @@ for i in range((datetime.now(pytz.timezone('America/Detroit')).date() - early_da
     logger.info("end")
     logger.info(end.astimezone().isoformat())
     
+    # post the report request job
     url = "https://indigo-api.bluejeans.com/v1/enterprise/" + str(ENV.get('BLUEJEANS_ENTERPRISE_ID')) + "/indigo/reportJob/meetings?filter=\
     [{\"type\":\"date\",\"comparison\":\"lt\",\"value\":\"" + end.astimezone().isoformat() + "\",\"field\":\"end_time\"},\
     {\"type\":\"date\",\"comparison\":\"lt\",\"value\":\"" + end.astimezone().isoformat() + "\",\"field\":\"start_time\"},\
     {\"type\":\"date\",\"comparison\":\"gt\",\"value\":\"" + start.astimezone().isoformat() + "\",\"field\":\"start_time\"}]\
     &fileName=meetings_30th-Mar-2020_30th-Mar-2020&userid=" + str(ENV.get("BLUEJEANS_USER_ID")) + "&app_name=command_center"
 
-    # post the report request job
     payload = {}
     headers = {
         'Accept': 'application/json',
@@ -171,6 +154,9 @@ for i in range((datetime.now(pytz.timezone('America/Detroit')).date() - early_da
             # append to the total output
             total_df = total_df.append(df)
             logger.info(df)
+
+            # remove file
+            os.remove(csv_file)
 
     # remove zip file
     os.remove(zip_file_name)
