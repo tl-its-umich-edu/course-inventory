0452815abafe653f82798263389f07037a9a18dd
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2020-04-07 14:46:44 -0400

    #65 - change MiVideo tables
    MySQL likes to use TIMESTAMP type columns for special purposes.  In order to use that type in these tables, extra settings needed to be set to either provide default values for those columns or let them be null.  With DATETIME, it's easier to specify they should not be null and leave it at that.

--- db/migrations/1138.add_mivideo_tables.py
+++ db/migrations/1138.add_mivideo_tables.py
@@ -2,31 +2,25 @@ from yoyo import step
 
 steps = [
     step('''
-        CREATE TABLE IF NOT EXISTS mivideo_media_started_hourly 
-        (
-            id BIGINT auto_increment NOT NULL UNIQUE,
-            event_hour_utc varchar(20) NOT NULL,
+        CREATE TABLE IF NOT EXISTS mivideo_media_started_hourly (
+            id BIGINT AUTO_INCREMENT NOT NULL UNIQUE,
+            event_hour_utc VARCHAR(20) NOT NULL,
             course_id BIGINT NOT NULL,
-            event_time_utc_latest TIMESTAMP NOT NULL,
+            event_time_utc_latest DATETIME NOT NULL,
             event_count BIGINT NOT NULL,
             PRIMARY KEY (id)
-        )
-        ENGINE=InnoDB
-        DEFAULT CHARSET=utf8mb4;
+        ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
     '''),
     step('''
-        CREATE TABLE IF NOT EXISTS mivideo_media_creation 
-        (
-            id BIGINT auto_increment NOT NULL UNIQUE,
-            media_id varchar(200) NOT NULL,
-            media_time_created_utc TIMESTAMP NOT NULL,
-            media_name varchar(200) NOT NULL,
+        CREATE TABLE IF NOT EXISTS mivideo_media_creation (
+            id BIGINT AUTO_INCREMENT NOT NULL UNIQUE,
+            media_id VARCHAR(200) NOT NULL,
+            media_time_created_utc DATETIME NOT NULL,
+            media_name VARCHAR(200) NOT NULL,
             media_duration_seconds BIGINT NOT NULL,
             course_id BIGINT NOT NULL,
-            event_time_utc_latest TIMESTAMP NOT NULL,
+            event_time_utc_latest DATETIME NOT NULL,
             PRIMARY KEY (id)
-        )
-        ENGINE=InnoDB
-        DEFAULT CHARSET=utf8mb4;
+        ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
     '''),
 ]
