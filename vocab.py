37d457f9d25c99389578585c79e364ae4221531e
Sam Sciolla <ssciolla@umich.edu>
2020-04-14 13:33:18 -0400

    Make enum comments docstrings

--- vocab.py
+++ vocab.py
@@ -4,19 +4,23 @@ from enum import auto, Enum
 
 # Enum(s)
 
-# Each job name should be defined in ValidJobName.
-# NAME_OF_JOB = 'path.to.method'
-
 class ValidJobName(Enum):
+    """
+    Each job name should be defined in ValidJobName.
+    NAME_OF_JOB = 'path.to.method'
+    """
+    
     COURSE_INVENTORY = 'course_inventory.inventory.run_course_inventory'
     # ONLINE_MEETINGS = 'online_meetings.report...'
     # ZOOM = 'online_meetings.canvas_zoom_meetings...'
     # MIVIDEO = 'mivideo...'
 
 
-# Each data source name should be defined in ValidDataSourceName.
-# NAME_OF_DATA_SOURCE = auto()
-
 class ValidDataSourceName(Enum):
+    """
+    Each data source name should be defined in ValidDataSourceName.
+    NAME_OF_DATA_SOURCE = auto()
+    """
+    
     CANVAS_API = auto()
     UNIZIN_DATA_WAREHOUSE = auto()
