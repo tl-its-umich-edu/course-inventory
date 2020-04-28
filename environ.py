72b2c53cfb4e907a8ef914fe2fcfcfb62476eb10
Sam Sciolla <ssciolla@umich.edu>
2020-04-28 17:13:59 -0400

    Add draft of env_schema.json; change to env.hjson; implement validation

--- environ.py
+++ environ.py
@@ -1,25 +1,32 @@
 # standard libraries
-import json
-import logging
-import os
+import json, logging, os, sys
 from json.decoder import JSONDecodeError
 from typing import Dict
+
+# third-party libraries
+import hjson
+from jsonschema import validate
+from jsonschema.exceptions import ValidationError
+
 
 # Set up ENV
 # entry-level job modules need to be one-level beneath root
 ROOT_DIR: str = os.path.dirname(os.path.abspath(__file__))
 CONFIG_DIR: str = os.path.join(ROOT_DIR, os.getenv('ENV_DIR', os.path.join('config', 'secrets')))
-CONFIG_PATH: str = os.path.join(CONFIG_DIR, os.getenv('ENV_FILE', 'env.json'))
+CONFIG_PATH: str = os.path.join(CONFIG_DIR, os.getenv('ENV_FILE', 'env.hjson'))
 
 logger = logging.getLogger(__name__)
 
 try:
     with open(CONFIG_PATH) as env_file:
-        ENV: Dict = json.loads(env_file.read())
+        ENV: Dict = hjson.loads(env_file.read())
 except FileNotFoundError:
     logger.error(
         f'Configuration file could not be found; please add file "{CONFIG_PATH}".')
     ENV = dict()
+
+with open(os.path.join(ROOT_DIR, 'config', 'env_schema.json')) as schema_file:
+    ENV_SCHEMA = json.loads(schema_file.read())
 
 LOG_LEVEL: str = ENV.get('LOG_LEVEL', 'INFO')
 logging.basicConfig(level=LOG_LEVEL)
@@ -39,3 +46,10 @@ for key, value in ENV.items():
         logger.info(f'key: {key}; os_value: {os_value}')
 
 logger.debug(ENV)
+
+try:
+    validate(instance=ENV, schema=ENV_SCHEMA)
+    logger.info('ENV is valid; the program will continue')
+except ValidationError:
+    logger.error('ENV in invalid; the program will exit')
+    sys.exit(1)
