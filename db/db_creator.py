0747baa836d3e013742543c51dadda668001c788
Sam Sciolla <35741256+ssciolla@users.noreply.github.com>
2020-05-04 15:06:11 -0400

    Refactor drop_records, remove APPEND_TABLE_NAMES (#72) (#129)
    * Remove append tables; let drop_records accept names; add doc strings
    
    * Update usages of DBCreator, specify tables in inventory
    
    * Remove APPEND_TABLE_NAMES from env_blank and README
    
    * Add logging to identify different drop_methods cases
    
    * Add detail about fluent interface to top docstring

--- db/db_creator.py
+++ db/db_creator.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 # standard libraries
 import logging, os
-from typing import Dict, List, Union
+from typing import Dict, List, Sequence, Union
 from urllib.parse import quote_plus
 
 # third-party libraries
@@ -18,18 +18,19 @@ logger = logging.getLogger(__name__)
 PARENT_PATH = os.path.dirname(os.path.abspath(__file__))
 MIGRATIONS_PATH = os.path.join(PARENT_PATH, 'migrations')
 
-# The metadata tables are append tables by default.
-DEFAULT_APPEND_TABLE_NAMES = ['job_run', 'data_source_status']
-
 
 class DBCreator:
+    '''
+    Utility class for managing the application's database. Leverages SQLAlchemy
+    and yoyo-migrations. The migrate, drop_records, and reset_database methods can be
+    used fluently, i.e. with method chaining (see reset_database for an example).
+    '''
 
-    def __init__(
-        self,
-        db_params: Dict[str, str],
-        append_table_names: Union[List[str], None] = None
-    ) -> None:
-
+    def __init__(self, db_params: Dict[str, str]) -> None:
+        '''
+        Sets the database name; sets the connection string; uses the connection string
+        to create a SQLAlchemy engine object.
+        '''
         self.db_name: str = db_params['dbname']
         self.conn_str: str = (
             'mysql+mysqldb' +
@@ -41,15 +42,18 @@ class DBCreator:
         )
         self.engine: Engine = create_engine(self.conn_str)
 
-        self.append_table_names: List[str] = DEFAULT_APPEND_TABLE_NAMES
-        if append_table_names is not None:
-            self.append_table_names += append_table_names
-
     def get_table_names(self) -> List[str]:
+        '''
+        Gets table names using the SQLAlchemy Engine object.
+        '''
         logger.debug('get_table_names')
         return self.engine.table_names()
 
     def migrate(self) -> DBCreator:
+        '''
+        Updates database schema using yoyo-migrations and the migration files in the
+        migrations directory collocated with this file (db_creator.py).
+        '''
         logger.debug('migrate')
         backend = get_backend(self.conn_str)
         migrations = read_migrations(MIGRATIONS_PATH)
@@ -57,18 +61,43 @@ class DBCreator:
             backend.apply_migrations(backend.to_apply(migrations))
         return self
 
-    def drop_records(self) -> DBCreator:
+    def drop_records(self, spec_table_names: Union[Sequence[str], None] = None) -> DBCreator:
+        '''
+        Drops records from either the specified database tables or all of the
+        application-managed database tables; tables managed by yoyo-migrations are ignored.
+        '''
         logger.debug('drop_records')
+        app_table_names = [
+            table_name for table_name in self.get_table_names() if 'yoyo' not in table_name
+        ]
+        logger.debug(f'app_table_names: {app_table_names}')
+
+        if spec_table_names is None:
+            # Drop all non-yoyo tables
+            logger.info('Dropping all application (non-yoyo) tables')
+            drop_table_names = app_table_names
+        else:
+            # Drop specified table names if they're valid (i.e. an application-managed table)
+            logger.info('Dropping specific provided tables if they are valid')
+            drop_table_names = []
+            for spec_table_name in spec_table_names:
+                if spec_table_name in app_table_names:
+                    drop_table_names.append(spec_table_name)
+                else:
+                    logger.error(f'Invalid table name was provided: {spec_table_name}')
+
         conn = self.engine.connect()
         conn.execute('SET FOREIGN_KEY_CHECKS=0;')
-        for table_name in self.get_table_names():
-            if 'yoyo' not in table_name and table_name not in self.append_table_names:
-                logger.debug(f'Table Name: {table_name}')
-                conn.execute(f'DELETE FROM {table_name};')
-                logger.info(f'Dropped records in {table_name} in {self.db_name}')
+        for drop_table_name in drop_table_names:
+            logger.debug(f'Table Name: {drop_table_name}')
+            conn.execute(f'DELETE FROM {drop_table_name};')
+            logger.info(f'Dropped records in {drop_table_name} in {self.db_name}')
         conn.execute('SET FOREIGN_KEY_CHECKS=1;')
         return self
 
-    def set_up_database(self) -> DBCreator:
+    def reset_database(self) -> DBCreator:
+        '''
+        Drops records in application-managed tables and applies outstanding migrations
+        '''
         self.drop_records().migrate()
         return self
