b0a679eee3f28070624019ccacc836de7d62f6cc
Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2020-05-18 12:34:03 -0400

    #142 - more pythonic
    Use `property()` decorators.  Removed `get*()` methods, but keep `set*()` ones for fluency/chaining.  Simplified `copy()`, and added string formatting to enum.

--- vocab.py
+++ vocab.py
@@ -35,25 +35,53 @@ class ValidDataSourceName(Enum):
     UNIZIN_DATA_PLATFORM_EVENTS = auto()
     UNIZIN_DATA_WAREHOUSE = auto()
 
+    def __str__(self):
+        '''
+        Return value name without `ValidDataSourceName` class name.
+
+        :return: Name of enum value.
+        '''
+        return self.name
+
+
 
 class DataSourceStatus:
     '''
     Each data source process must return an instance of this class, which tells the name of the
     data source and the time at which data from it was last updated.
     '''
+
     def __init__(
             self,
             dataSourceName: ValidDataSourceName = NotImplemented,
             dataUpdatedAt: datetime = NotImplemented
     ) -> None:
         '''
+        Create a data source status object with name and update timestamp.
 
         :param dataSourceName: `ValidDataSourceName` - Name of data source
-        :param dataUpdatedAt: `datetime` - Time at which data from the source was updated
+        :param dataUpdatedAt: `datetime` - Time when data from the source was updated
         '''
-        self.dataSourceName: ValidDataSourceName
-        self.dataUpdatedAt: datetime
+        self._dataSourceName: ValidDataSourceName
+        self._dataUpdatedAt: datetime
+        self.setDataSourceName(dataSourceName).setDataUpdatedAt(dataUpdatedAt)
+
+    @property
+    def dataSourceName(self) -> ValidDataSourceName:
+        '''Name of the data source'''
+        return self._dataSourceName
+
+    @dataSourceName.setter
+    def dataSourceName(self, dataSourceName: ValidDataSourceName = NotImplemented):
         self.setDataSourceName(dataSourceName)
+
+    @property
+    def dataUpdatedAt(self) -> datetime:
+        '''Time when data from the source was updated'''
+        return self._dataUpdatedAt
+
+    @dataUpdatedAt.setter
+    def dataUpdatedAt(self, dataUpdatedAt: datetime):
         self.setDataUpdatedAt(dataUpdatedAt)
 
     def setDataSourceName(
@@ -61,7 +89,7 @@ class DataSourceStatus:
             dataSourceName: ValidDataSourceName = NotImplemented
     ) -> DataSourceStatus:
         '''
-        Set name of data source
+        Set name of data source.  Main logic and fluent interface.
 
         :param dataSourceName: `ValidDataSourceName` - Name of data source
         :return: `DataSourceStatus`
@@ -71,49 +99,32 @@ class DataSourceStatus:
         if (not isinstance(dataSourceName, ValidDataSourceName)):
             raise ValueError('dataSourceName must be of type ValidDataSourceName')
 
-        self.dataSourceName = dataSourceName
+        self._dataSourceName = dataSourceName
         return self
-
-    def getDataSourceName(self) -> ValidDataSourceName:
-        '''
-        Get name of data source
-
-        :return: `ValidDataSourceName` - Name of data source
-        '''
-        return self.dataSourceName
 
     def setDataUpdatedAt(self, dataUpdatedAt: datetime = NotImplemented) -> DataSourceStatus:
         '''
-        Set time data source updated
+        Set time data source updated.  Main logic and fluent interface.
 
         :param dataUpdatedAt: `datetime` - Time data source updated
         :return: `DataSourceStatus`
         '''
         if (dataUpdatedAt is NotImplemented):
-            self.dataUpdatedAt = pd.to_datetime(time.time(), unit='s', utc=True)
+            self._dataUpdatedAt = pd.to_datetime(time.time(), unit='s', utc=True)
         elif (dataUpdatedAt.tzinfo is not UTC):
-            raise ValueError('dataUpdatedAt must have UTC time zone')
+            raise ValueError('_dataUpdatedAt must have UTC time zone')
         else:
-            self.dataUpdatedAt = dataUpdatedAt
+            self._dataUpdatedAt = dataUpdatedAt
         return self
-
-    def getDataUpdatedAt(self) -> datetime:
-        '''
-        Get time data source updated
-
-        :return: `datetime` -  Time data source updated
-        '''
-        return self.dataUpdatedAt
 
     def copy(self) -> Dict:
         '''
-        Typical Pythonic method to return a dictionary representation of an object.  Note that the
-        `data_source_name` key contains `dataSourceName.name`, not a `ValidDataSourceName` object.
+        Return dictionary representation of DataSourceStatus object.
 
         :return: `Dict` of object properties
         '''
 
         return {
-            'data_source_name': self.dataSourceName.name,
-            'data_updated_at': self.dataUpdatedAt
+            'data_source_name': self._dataSourceName,
+            'data_updated_at': self._dataUpdatedAt
         }
