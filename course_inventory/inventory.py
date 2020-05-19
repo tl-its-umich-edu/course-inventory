07bd9a3cd3af4851203921439497de82eebe907f
Pushyami Gundala <pushyamig@users.noreply.github.com>
2020-05-19 13:16:22 -0400

    Update course_inventory/inventory.py
    Co-authored-by: Sam Sciolla <35741256+ssciolla@users.noreply.github.com>

--- course_inventory/inventory.py
+++ course_inventory/inventory.py
@@ -266,7 +266,7 @@ def run_course_inventory() -> Sequence[DataSourceStatus]:
     logger.info(f"""Size of Published courses from API:
                 {course_copy_df[(course_copy_df['workflow_state'] == 'available')].shape}""")
     course_from_db_df = get_course_info_from_DB(db_creator_obj)
-    logger.info(f"Size of Published courses from DB with:  {course_from_db_df.shape}")
+    logger.info(f"Size of published courses from DB with: {course_from_db_df.shape}")
     published_date_in_db = course_from_db_df[(course_from_db_df['published_at'].notnull())].shape
     logger.info(f"Size of Published courses from DB with published date: {published_date_in_db}")
     course_copy_df = pd.merge(course_copy_df, course_from_db_df, on='canvas_id', how='left')
