93885ed20ebdb9cf708f9a87b5d6afb1fa879b8d
Lance E Sloan «UMich» <lsloan@umich.edu>
2020-06-15 16:43:30 -0400

    define MiVideo table relationships (#183) (#194)

---
+++ db/migrations/0021.add_mivideo_fk.py
@@ -0,0 +1,18 @@
+'''
+Migration to add MiVideo table foreign key
+'''
+
+from yoyo import step
+
+__depends__ = {'0016.add_mivideo_creation_tables'}
+
+steps = [
+    step('''
+        ALTER TABLE mivideo_media_courses 
+          ADD CONSTRAINT fk_media_id 
+          FOREIGN KEY (media_id)
+          REFERENCES mivideo_media_created(id)
+          ON UPDATE CASCADE
+          ON DELETE CASCADE;
+    '''),
+]
