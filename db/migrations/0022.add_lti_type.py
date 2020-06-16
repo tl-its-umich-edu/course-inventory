cd417eb096cb0ffafd8bcf2a8ce4b1e17209d8df
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2020-06-16 17:13:27 -0400

    Fixing up the foreign keys in the conversion

--- db/migrations/0022.add_lti_type.py
+++ db/migrations/0022.add_lti_type.py
@@ -7,17 +7,18 @@ from yoyo import step
 __depends__ = {'0020.add_lti_usage'}
 
 steps = [
-    step('''
-        ALTER TABLE lti_placement ALTER COLUMN placement_type_id INTEGER NULL;
-    '''),
-    step('''
+   step('''
         CREATE TABLE IF NOT EXISTS lti_type (
-            canvas_id BIGINT NOT NULL,
+            canvas_id INTEGER NOT NULL,
             des VARCHAR(200),
             rpt_group_nm VARCHAR(200),
-            PRIMARY KEY (canvas_id),
-            FOREIGN KEY (canvas_id) REFERENCES lti_placement(placement_type_id) ON DELETE SET NULL ON UPDATE CASCADE
+            PRIMARY KEY (canvas_id)
         ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+    '''),
+    step('''
+        ALTER TABLE lti_placement MODIFY placement_type_id INTEGER NULL,
+        ADD FOREIGN KEY (placement_type_id) REFERENCES lti_type(canvas_id) 
+        ON DELETE SET NULL ON UPDATE CASCADE
     '''),
     step('''
         INSERT INTO lti_type (canvas_id, des, rpt_group_nm)
